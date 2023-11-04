573cf09232f486a5391deb0629aace8e1ffa91ee
Ashish Goyal <ashishgy77@gmail.com>
2023-11-04 13:19:00 +0530

    adding .gitignore file and adding code for get_dataloader method of data_base.py

--- nn_builder/toolkit/data_base.py
+++ nn_builder/toolkit/data_base.py
@@ -1,3 +1,4 @@
+import torch
 from nn_builder.toolkit.utils import (
     HyperParameters,
 )
@@ -8,10 +9,18 @@ class DataModule(HyperParameters):  #@save
         self.save_hyperparameters()
 
     def get_dataloader(self, train):
-        raise NotImplementedError
+        i = slice(0, self.num_train) if train else slice(self.num_train, None)
+        return self.get_tensorloader((self.X, self.y), train, i)
 
     def train_dataloader(self):
         return self.get_dataloader(train=True)
 
     def val_dataloader(self):
         return self.get_dataloader(train=False)
+
+    def get_tensorloader(self, tensors, train, indices=slice(0, None)):
+        tensors = tuple(a[indices] for a in tensors)
+        dataset = torch.utils.data.TensorDataset(*tensors)
+        return torch.utils.data.DataLoader(dataset, self.batch_size,shuffle=train)
+
+
