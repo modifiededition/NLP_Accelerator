b82c20294541c30b49b7cea9dd6df2d482371fd1
Ashish Goyal <ashishgy77@gmail.com>
2023-11-04 17:02:38 +0530

    Adding SGD optimizer

---
+++ nn_builder/optimizers/sgd.py
@@ -0,0 +1,15 @@
+from nn_builder.toolkit.utils import HyperParameters
+
+class SGD(HyperParameters):  #@save
+    """Minibatch stochastic gradient descent."""
+    def __init__(self, params, lr):
+        self.save_hyperparameters()
+
+    def step(self):
+        for param in self.params:
+            param -= self.lr * param.grad
+
+    def zero_grad(self):
+        for param in self.params:
+            if param.grad is not None:
+                param.grad.zero_()
