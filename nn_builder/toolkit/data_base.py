dbe624f6a10d8e4c71219908443e1d65333c296b
Ashish Goyal <ashishgy77@gmail.com>
2023-11-04 11:34:29 +0530

    building structure in the library!

---
+++ nn_builder/toolkit/data_base.py
@@ -0,0 +1,17 @@
+from nn_builder.toolkit.utils import (
+    HyperParameters,
+)
+
+class DataModule(HyperParameters):  #@save
+    """The base class of data."""
+    def __init__(self, root='../data', num_workers=4):
+        self.save_hyperparameters()
+
+    def get_dataloader(self, train):
+        raise NotImplementedError
+
+    def train_dataloader(self):
+        return self.get_dataloader(train=True)
+
+    def val_dataloader(self):
+        return self.get_dataloader(train=False)
