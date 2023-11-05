a6568ab1a4116c6928e7ad7e1543950211b70841
Ashish Goyal <ashishgy77@gmail.com>
2023-11-05 17:06:12 +0530

    Adding Linear Regression Architecture with concise implmentation

---
+++ nn_builder/archetectures/linear_regression/linearRegressionConcise.py
@@ -0,0 +1,23 @@
+import torch
+
+from nn_builder.toolkit.models_base import Module
+
+
+class LinearRegression(Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.save_hyperparameters()
+        self.net = torch.nn.LazyLinear(1)
+        self.net.weight.data.normal_(0, 0.01)
+        self.net.bias.data.fill_(0)
+
+    def loss(self, y_hat, y):
+        fn = torch.nn.MSELoss()
+        return fn(y_hat, y)
+
+    def forward(self, X):
+        return self.net(X)
+
+    def configure_optimizers(self):
+        return torch.optim.SGD(self.parameters(), self.lr)
