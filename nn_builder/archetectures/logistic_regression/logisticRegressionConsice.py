59afc1ca6cc8130f8fa5e30de73e50261cf21140
Ashish Goyal <ashishgy77@gmail.com>
2023-11-07 16:37:51 +0530

    adding Logistic regression archetecture consice

---
+++ nn_builder/archetectures/logistic_regression/logisticRegressionConsice.py
@@ -0,0 +1,20 @@
+from nn_builder.toolkit.classification_base import Classifier
+from torch import nn
+from torch.nn import functional as F
+
+
+class LogisticRegression(Classifier):  #@save
+    """The softmax regression model."""
+    def __init__(self, num_outputs, lr):
+        super().__init__()
+        self.save_hyperparameters()
+        self.net = nn.Sequential(nn.Flatten(),
+                                 nn.LazyLinear(num_outputs))
+
+    def forward(self, X):
+        return self.net(X)
+
+    def loss(self, Y_hat, Y, averaged=True):
+        Y_hat = Y_hat.reshape((-1, Y_hat.shape[-1]))
+        Y = Y.reshape((-1,))
+        return F.cross_entropy(Y_hat, Y, reduction='mean' if averaged else 'none')
