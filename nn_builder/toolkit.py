6ec4ea4586a1f13f8c15eeb521517ce81091d58f
Ashish Goyal <ashishgy77@gmail.com>
2023-11-17 08:25:13 +0530

    c-2 restructure toolkit add support of gpu in trainer base class

---
+++ nn_builder/toolkit.py
@@ -0,0 +1,165 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from nn_builder.utils import (
+    HyperParameters,
+    ProgressBoard,
+    get_num_gpus,
+    gpu,
+)
+
+###################################### Base Data Loader Class ##########################
+class DataModule(HyperParameters):  #@save
+    """The base class of data."""
+    def __init__(self, root='../data', num_workers=4):
+        self.save_hyperparameters()
+
+    def get_dataloader(self, train):
+        i = slice(0, self.num_train) if train else slice(self.num_train, None)
+        return self.get_tensorloader((self.X, self.y), train, i)
+
+    def train_dataloader(self):
+        return self.get_dataloader(train=True)
+
+    def val_dataloader(self):
+        return self.get_dataloader(train=False)
+
+    def get_tensorloader(self, tensors, train, indices=slice(0, None)):
+        tensors = tuple(a[indices] for a in tensors)
+        dataset = torch.utils.data.TensorDataset(*tensors)
+        return torch.utils.data.DataLoader(dataset, self.batch_size,shuffle=train)
+
+###################################### Base Model Class ##########################
+
+class Module(nn.Module, HyperParameters):
+    """ The base class for all the models"""
+
+    def __init__(self, plot_train_per_epoch = 2, plot_valid_per_epoch  = 1) -> None:
+        super().__init__()
+        self.save_hyperparameters()
+        self.board = ProgressBoard()
+
+    def loss(self, y_hat, y):
+        raise NotImplementedError
+
+    def forward(self, X):
+        assert hasattr(self, 'net')
+        return self.net(X)
+
+    def plot(self, key, value, train):
+        """Plot a point in animation."""
+        assert hasattr(self, 'trainer'), 'Trainer is not inited'
+        self.board.xlabel = 'epoch'
+
+        if train:
+            x = self.trainer.train_batch_idx / self.trainer.num_train_batches
+            n = self.trainer.num_train_batches / self.plot_train_per_epoch
+        else:
+            x = self.trainer.epoch + 1
+            n = self.trainer.num_val_batches / self.plot_valid_per_epoch
+
+        self.board.draw(x, value.to('cpu').detach().numpy(),
+                        ('train_' if train else 'val_') + key,
+                        every_n=int(n))
+
+    def training_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+        self.plot('loss', l, train=True)
+        return l
+
+    def validation_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+        self.plot('loss', l, train=False)
+
+    def configure_optimizers(self):
+        raise NotImplementedError
+
+###################################### Base Trainer Class ##########################
+
+
+class Trainer(HyperParameters):
+
+    def __init__(self, max_epochs, num_gpus = 0, gradient_clip_val  = 0) -> None:
+        self.save_hyperparameters()
+        self.gpus = [gpu(i) for i in range(min(num_gpus, get_num_gpus()))]
+
+    def prepare_data(self, data):
+        self.train_dataloader = data.train_dataloader()
+        self.val_dataloader = data.val_dataloader()
+        self.num_train_batches = len(self.train_dataloader)
+        self.num_val_batches = (len(self.val_dataloader)
+                                if self.val_dataloader is not None else 0)
+
+    def prepare_model(self, model):
+        model.trainer = self
+        model.board.xlim = [0, self.max_epochs]
+
+        if self.gpus:
+            model.to(self.gpus[0])
+        self.model = model
+
+    def prepare_batch(self, batch):
+        if self.gpus:
+            batch = [a.to(self.gpus[0]) for a in batch]
+        return batch
+
+    def fit(self, model, data):
+        self.prepare_data(data)
+        self.prepare_model(model)
+
+        self.optim = model.configure_optimizers()
+        self.epoch= 0
+        self.train_batch_idx = 0
+        self.val_batch_idx = 0
+        for self.epoch in range(self.max_epochs):
+            self.fit_epoch()
+
+    def fit_epoch(self):
+        self.model.train()
+        for batch in self.train_dataloader:
+            loss = self.model.training_step(self.prepare_batch(batch))
+            self.optim.zero_grad()
+            with torch.no_grad():
+                loss.backward()
+                if self.gradient_clip_val > 0:
+                    self.clip_gradients(self.gradient_clip_value, self.model)
+                self.optim.step()
+            self.train_batch_idx +=1
+
+        if self.val_dataloader is None:
+            return
+        self.model.eval()
+
+        for batch in self.val_dataloader:
+            with torch.no_grad():
+                self.model.validation_step(self.prepare_batch(batch))
+            self.val_batch_idx +=1
+
+
+################# Classification Class #####################################################
+
+class Classifier(Module):  #@save
+    """The base class of classification models."""
+    def validation_step(self, batch):
+        Y_hat = self(*batch[:-1])
+        self.plot('loss', self.loss(Y_hat, batch[-1]), train=False)
+        self.plot('acc', self.accuracy(Y_hat, batch[-1]), train=False)
+
+    def configure_optimizers(self):
+        return torch.optim.SGD(self.parameters(), lr=self.lr)
+
+
+    def accuracy(self, Y_hat, Y, averaged=True):
+        """Compute the number of correct predictions."""
+        Y_hat = Y_hat.reshape((-1, Y_hat.shape[-1]))
+        preds = Y_hat.argmax(axis=1).type(Y.dtype)
+        compare = (preds == Y.reshape(-1)).type(torch.float32)
+        return compare.mean() if averaged else compare
+
+    def loss(self, Y_hat, Y, averaged=True):
+        Y_hat = Y_hat.reshape((-1, Y_hat.shape[-1]))
+        Y = Y.reshape((-1,))
+        return F.cross_entropy(Y_hat, Y, reduction='mean' if averaged else 'none')
+
+
---
+++ nn_builder/utils.py
@@ -0,0 +1,154 @@
+import inspect
+import collections
+from IPython import display
+from matplotlib_inline import backend_inline
+import matplotlib.pyplot as plt
+import torch
+
+
+
+
+def cpu():  #@save
+    """Get the CPU device."""
+    return torch.device('cpu')
+
+def gpu(i=0):  #@save
+    """Get a GPU device."""
+    return torch.device(f'cuda:{i}')
+
+def get_num_gpus():  #@save
+    """Get the number of available GPUs."""
+    return torch.cuda.device_count()
+
+def try_gpu(i=0):  #@save
+    """Return gpu(i) if exists, otherwise return cpu()."""
+    if get_num_gpus() >= i + 1:
+        return gpu(i)
+    return cpu()
+
+def try_all_gpus():  #@save
+    """Return all available GPUs, or [cpu(),] if no GPU exists."""
+    return [gpu(i) for i in range(get_num_gpus())]
+
+
+def add_to_class(Class):
+    """Register funtion as method in the original class"""
+
+    def wrapper(obj):
+        setattr(Class,obj.__name__, obj)
+
+    return wrapper
+
+
+class HyperParameters:
+    """The base class of hyperparameters."""
+    def save_hyperparameters(self, ignore=[]):
+        """Save function arguments into class attributes.
+
+        Defined in :numref:`sec_utils`"""
+        frame = inspect.currentframe().f_back
+        _, _, _, local_vars = inspect.getargvalues(frame)
+        self.hparams = {k:v for k, v in local_vars.items()
+                        if k not in set(ignore+['self']) and not k.startswith('_')}
+        for k, v in self.hparams.items():
+            setattr(self, k, v)
+
+
+def use_svg_display():
+    """Use the svg format to display a plot in Jupyter.
+
+    Defined in :numref:`sec_calculus`"""
+    backend_inline.set_matplotlib_formats('svg')
+
+
+class ProgressBoard(HyperParameters):
+
+    """ Board to display the line chart."""
+
+    def __init__(self, xlabel=None,ylabel = None, xlim=None, ylim=None,
+                  xscale = 'linear', yscale = 'linear',
+                  ls=['-', '--', '-.', ':'], colors=['C0', 'C1', 'C2', 'C3'],
+                  fig=None, axes=None, figsize=(3.5, 2.5), display=True):
+
+         self.save_hyperparameters()
+
+    def draw(self, x,y,label, every_n = 1):
+
+        Point = collections.namedtuple('Point',['x','y'])
+
+        if not hasattr(self, 'raw_points'):
+            self.raw_points = collections.OrderedDict()
+            self.data = collections.OrderedDict()
+
+        if label not in self.raw_points:
+            self.raw_points[label] = []
+            self.data[label] = []
+
+        points = self.raw_points[label]
+        line = self.data[label]
+
+        points.append(Point(x, y))
+        if len(points) != every_n:
+            return
+
+        mean = lambda x: sum(x) / len(x)
+
+        line.append( Point(mean([p.x for p in points]),
+                        mean([p.y for p in points]))
+                )
+
+        points.clear()
+        if not self.display:
+            return
+
+        use_svg_display()
+
+        if self.fig is None:
+            self.fig = plt.figure(figsize=self.figsize)
+
+        plt_lines, labels = [], []
+
+        for (k, v), ls, color in zip(self.data.items(), self.ls, self.colors):
+                plt_lines.append(plt.plot([p.x for p in v], [p.y for p in v],
+                                            linestyle=ls, color=color)[0])
+                labels.append(k)
+
+        axes = self.axes if self.axes else plt.gca()
+
+        if self.xlim:
+            axes.set_xlim(self.xlim)
+
+        if self.ylim:
+            axes.set_ylim(self.ylim)
+
+        if not self.xlabel:
+            self.xlabel = self.x
+
+        axes.set_xlabel(self.xlabel)
+        axes.set_ylabel(self.ylabel)
+        axes.set_xscale(self.xscale)
+        axes.set_yscale(self.yscale)
+        axes.legend(plt_lines, labels)
+
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
+    """Plot a list of images.
+
+    Defined in :numref:`sec_utils`"""
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        try:
+            img = img.to('cpu').detach().numpy()
+        except:
+            pass
+        ax.imshow(img)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    return axes
